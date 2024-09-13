17c4281d01d481eedba13943ae1aa5833c11cf55
dennishenle <henledennis@gmail.com>
2024-09-13 18:53:52 +0200

    initial commit

---
+++ myApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myApplication
+//
+//  Created by Dennis Henle on 13.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myApplication/myApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  myApplicationApp.swift
+//  myApplication
+//
+//  Created by Dennis Henle on 13.09.24.
+//
+
+import SwiftUI
+
+@main
+struct myApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
